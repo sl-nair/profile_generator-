841984c34024a15b0a3836e302871b40417f23a8
Sethu Nair <sethunair2102@gmail.com>
2023-04-20 18:30:04 -0600

    used readline to create profile generator

---
+++ survey.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
+  // console.log(`${answer}`);
+  rl.question('What\'s an activity you like doing?', (activity) => {
+    rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)' , (meal) => {
+      rl.question('What\'s your favourite thing to eat for that meal?' , (food) => {
+        console.log (`${name} likes ${activity}, and having ${food} for ${meal}!`);
+        rl.close();
+      });
+    });
+  });
+});
+
