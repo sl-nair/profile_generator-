b987f7a28826d6d6a4f7bba074631a187c3d6e48
Sethu Nair <sethunair2102@gmail.com>
2023-04-20 18:35:34 -0600

    removed unnecessary lines of code

--- survey.js
+++ survey.js
@@ -6,7 +6,6 @@ const rl = readline.createInterface({
 });
 
 rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
-  // console.log(`${answer}`);
   rl.question('What\'s an activity you like doing?', (activity) => {
     rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)' , (meal) => {
       rl.question('What\'s your favourite thing to eat for that meal?' , (food) => {
